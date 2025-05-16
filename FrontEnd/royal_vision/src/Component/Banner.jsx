8874ee590bd881599ede7c29e9faba63bba87057
Muhammad Hammad <127588251+Hammad-12345@users.noreply.github.com>
2025-05-16 23:52:45 +0500

    push

--- FrontEnd/royal_vision/src/Component/Banner.jsx
+++ FrontEnd/royal_vision/src/Component/Banner.jsx
@@ -6,14 +6,15 @@ const Banner = () => {
     <div className="relative w-full h-screen overflow-hidden">
       {/* Video Background */}
       <video
-        src={"changes.mp4"}
         autoPlay
         muted
         loop
         playsInline
         preload="auto"
         className="absolute inset-0 w-full h-full object-cover"
-      />
+      >
+         <source src="changes.mp4" type="video/mp4" />
+      </video>
 
       {/* Blend Overlay */}
       <div className="absolute inset-0 bg-black bg-opacity-50 mix-blend-multiply"></div>
