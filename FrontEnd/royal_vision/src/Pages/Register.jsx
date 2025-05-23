24082a23b2f775b70367b6ecfb08eb0ae6ec4ddd
Rana Moawaz <127588251+Hammad-12345@users.noreply.github.com>
2025-05-23 10:49:44 +0500

    heruko push

--- FrontEnd/royal_vision/src/Pages/Register.jsx
+++ FrontEnd/royal_vision/src/Pages/Register.jsx
@@ -59,7 +59,7 @@ export default function Register() {
     const fullData = { ...data, CountryPhoneCode: phoneCode };
 
     try {
-      const response = await fetch("http://api.overlandsolutions.net/auth/register", {
+      const response = await fetch("https://overland-backend-928cfa309f6f.herokuapp.com/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
